448b98c11f94d52df3c715d60f323ab67d50ebfe
Abdul Shabbir <abdulqshabbir@gmail.com>
2022-04-17 16:27:45 +0800

    reverse-string solution added

--- daily-byte-challenges/001-reverse-string.js
+++ daily-byte-challenges/001-reverse-string.js
@@ -9,3 +9,13 @@ Ex: Given the following strings...
 “The Daily Byte”, return "etyB yliaD ehT”
 “civic”, return “civic”
 */
+
+// Author: Abdul
+function reverseString(str) {
+    return [...str].reverse().join("")
+}
+
+// will log "Assertion failed" if test fails and nothing if test passes
+console.assert(reverseString("Cat") === "taC")
+console.assert(reverseString("The Daily Byte") === "etyB yliaD ehT")
+console.assert(reverseString("civic") === "civic")
